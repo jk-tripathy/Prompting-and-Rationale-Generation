b88f3c75a7fd77c68b68050cece65641b2ef15ef
Jatin <jatin.karthik.tripathy@uni-potsdam.de>
2022-11-20 12:00:09 +0000

    GPT-j-6b text only and outputs
    broken. results differ from using the online api

---
+++ main.py
@@ -0,0 +1,50 @@
+from data.load_PCL import load_binary, load_multi
+from pipeline import local_inf
+import json
+
+def split_data(df):
+    if df['label'].dtype != object:
+        return [y for x, y in df.groupby(['label'], axis=0)]
+    else:
+        pass
+
+def prompt_gen(dfs, class_dict, prompt_len=1, val_len=1):
+    train_prompts = 'Answer with Yes or No.\n'
+    val_prompts = []
+    for i, df in enumerate(dfs):
+        train_df = df[:int(len(df)*0.8)]
+        val_df = df[-val_len:]
+        for j in range(prompt_len):
+            inp = f'Prompt: {train_df.iloc[j]["text"]} Is this Patronizing?\n'
+            inp = inp + f'Answer: {class_dict[i]}.\n'
+            train_prompts = train_prompts + inp
+        for j in range(val_len):
+            inp = f'Prompt: {val_df.iloc[j]["text"]} Is this Patronizing?\n'
+            inp = inp + f'Answer: '
+            val_prompts.append(inp)
+    prompts = []
+    for elem in val_prompts:
+        prompts.append(train_prompts + f'{elem}')
+    return prompts
+        
+if __name__=='__main__':
+    df = load_binary()
+    df_split = split_data(df)
+    model_id = 'EleutherAI/gpt-j-6B'
+    #for i in [1, 2, 5, 10, 15, 20]:
+        #prompts = prompt_gen(df_split, {0:"No", 1:"Yes"}, prompt_len=1, val_len=5)
+        #responses = local_inf(model_id, prompts, max_new_tokens=1, batch_size=5)
+        #with open(f'outputs/main_prompt_batched_prompt_len_{i}.json', 'w', encoding='utf-8') as f:
+            #json.dump(responses, f, ensure_ascii=False, indent=4)
+        #for resp in responses:
+            #print(resp[0]['generated_text'])
+
+    prompt = [
+        "Prompt: We 're living in times of absolute insanity , as I 'm pretty sure most people are aware . For a while , waking up every day to check the news seemed to carry with it the same feeling of panic and dread that action heroes probably face when they 're trying to decide whether to cut the blue or green wire on a ticking bomb -- except the bomb 's instructions long ago burned in a fire and imminent catastrophe seems the likeliest outcome . It 's hard to stay that on-edge for that long , though , so it 's natural for people to become inured to this constant chaos , to slump into a malaise of hopelessness and pessimism . Is this Patronizing?\nAnswer: No.",
+        "Prompt: Arshad said that besides learning many new aspects of sports leadership he learnt how fast-developing nations were using sports as a tool of development and in this effort the disabled and the underprivileged were not left behind at any stage . Is this Patronizing?\nAnswer: Yes.",
+        "Prompt: He added that the AFP will continue to bank on the application of the whole of nation -- whole of government approach , which involves the use of sustainable and peaceful engagements with vulnerable communities to protect them from further NPA exploitation . Is this Patronizing?\nAnswer: "
+    ]
+    resp = local_inf(model_id, prompt, max_new_tokens=1, batch_size=1)
+    print(resp)
+    
+
--- pipeline.py
+++ pipeline.py
@@ -4,23 +4,44 @@ from time import time
 from transformers import pipeline
 
 
-def local_inf(model_id, prompt, temperature=0.7, top_p=None, max_new_tokens=32, repetition_penalty=None, do_sample=False, num_return_sequences=1):
+def local_inf(model_id, prompts, temperature=0.9, max_new_tokens=32, do_sample=True, batch_size=5):
     #model_id = 'bigscience/bloom'
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", offload_folder="offload", torch_dtype=torch.float16)
+    model_id = 'EleutherAI/gpt-j-6B'
+    model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", offload_folder="offload")
+    tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side='left')
+    tokenizer.pad_token = tokenizer.eos_token
 
-    pipe = pipeline('text-generation',model=model, tokenizer=tokenizer, torch_dtype=torch.float16, device=0)
-    response = pipe(
-        f"{prompt}",
-        temperature = temperature, # 0 to 1
-        top_p = top_p, # None, 0-1
-        max_new_tokens = max_new_tokens, # up to 2047 theoretically
-        return_full_text = False, # include prompt or not.
-        repetition_penalty = repetition_penalty, # None, 0-100 (penalty for repeat tokens.
-        do_sample = do_sample, # True: use sampling, False: Greedy decoding.
-        num_return_sequences = num_return_sequences
+    pipe = pipeline(
+        'text-generation',
+        model=model, 
+        tokenizer=tokenizer, 
+        device=0, 
+        batch_size=batch_size, 
     )
-    return response[0]['generated_text']
+    pipe.pad_token_id = pipe.model.config.eos_token_id
+    response = pipe(
+        prompts,
+        #temperature = temperature, # 0 to 1
+        #max_new_tokens = max_new_tokens, # up to 2047 theoretically
+        #do_sample = do_sample, # True: use sampling, False: Greedy decoding.
+    )
+    return response
+    #else:
+        #model = AutoModelForCausalLM.from_pretrained(model_id)
+        #tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side='left')
+        #tokenizer.pad_token = tokenizer.eos_token
+
+        #responses = []
+        #for prompt in prompts:
+            #input_ids = tokenizer(prompt, return_tensors="pt", padding=True)
+            #gen_tokens = model.generate(
+                #**input_ids,
+                #do_sample=True,
+                #temperature=0.9,
+                #max_new_tokens=max_new_tokens
+            #)
+            #responses.append(tokenizer.batch_decode(gen_tokens)[0])
+        #return responses
 
 if __name__=='__main__':
     model_id = 'EleutherAI/gpt-j-6B'
