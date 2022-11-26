e069cafebefaf1702f9530b2e7dde69f6d4ea860
Jatin Karthik Tripathy <jatin.karthik.tripathy@uni-potsdam.de>
2022-11-26 11:22:56 +0000

    added default params to text pipeline

--- Stage1/txt_pipeline.py
+++ Stage1/txt_pipeline.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import pipeline
 
 
-def text_pipeline(model_id, prompts, temperature=0.9, max_new_tokens=32, do_sample=True, batch_size=5):
+def text_pipeline(model_id, prompts, temperature=0.7, max_new_tokens=32, do_sample=False, batch_size=5):
     model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", offload_folder="offload")
     tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side='left')
     tokenizer.pad_token = tokenizer.eos_token
@@ -17,9 +17,9 @@ def text_pipeline(model_id, prompts, temperature=0.9, max_new_tokens=32, do_samp
     pipe.pad_token_id = pipe.model.config.eos_token_id
     response = pipe(
         prompts,
-        #temperature = temperature, # 0 to 1
-        #max_new_tokens = max_new_tokens, # up to 2047 theoretically
-        #do_sample = do_sample, # True: use sampling, False: Greedy decoding.
+        temperature = temperature, # 0 to 1
+        max_new_tokens = max_new_tokens, # up to 2047 theoretically
+        do_sample = do_sample, # True: use sampling, False: Greedy decoding.
     )
     return response
 
