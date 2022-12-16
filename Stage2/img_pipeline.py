1367d835de71539cb84e922c5dbf426d0c4a1693
Jatin Karthik Tripathy <jatin.karthik.tripathy@uni-potsdam.de>
2022-12-16 06:34:24 +0000

    Major Revision
    - changed the output formatting in jsonl file
    - refactored img_pipeline to be a class to avoid reloading all the models
    - added places365 for outdoor scene classifications

--- Stage2/img_pipeline.py
+++ Stage2/img_pipeline.py
@@ -1,28 +1,144 @@
+import sys
+sys.path.insert(0, "../")
 from transformers import pipeline, AutoFeatureExtractor, AutoModelForImageClassification, AutoModelForObjectDetection, AutoModelForVision2Seq, AutoTokenizer
+import torch
+from torch.autograd import Variable as V
+import torchvision.models as models
+from torchvision import transforms as trn
+from torch.nn import functional as F
+from PIL import Image
+import clip
 
-def image_pipeline(image, model_id, task):
-    feature_extractor = AutoFeatureExtractor.from_pretrained(model_id)
-    if task == 'object-detection':
-        model = AutoModelForObjectDetection.from_pretrained(model_id)
-    elif task == 'image-classification':
-        model = AutoModelForImageClassification.from_pretrained(model_id)
-    elif task == 'image-to-text':
-        model = AutoModelForVision2Seq.from_pretrained(model_id)
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
+import warnings
+warnings.filterwarnings("ignore")
 
-    pipe = pipeline(
-        task,
-        model = model,
-        feature_extractor = feature_extractor,
-        tokenizer = tokenizer if task == 'image-to-text' else None,
-    )
-    results = pipe(image)
-    return results
+#from PIL import Image
+#import torch
+#from torchvision import transforms
+#from transformers import OFATokenizer, OFAModel
+#from OFA.models import sequence_generator
+#from modelscope.pipelines import pipeline
+#from modelscope.utils.constant import Tasks
+#from modelscope.outputs import OutputKeys
+
+class ImagePipelines:
+    def __init__(self) -> None:
+        self.tasks = ['object detection', 'indoor scene', 'outdoor scene', 'facial emotion', 'caption']
+        #huggging face models
+        # object detection
+        self.object_model_id = 'facebook/detr-resnet-101'
+        self.object_feature_extractor = AutoFeatureExtractor.from_pretrained(self.object_model_id)
+        self.object_model = AutoModelForObjectDetection.from_pretrained(self.object_model_id)
+        self.object_pipe = pipeline(
+            'object-detection',
+            model = self.object_model,
+            feature_extractor = self.object_feature_extractor,
+        )
+
+        #indoor scene
+        self.indoor_model_id = 'vincentclaes/mit-indoor-scenes'
+        self.indoor_feature_extractor = AutoFeatureExtractor.from_pretrained(self.indoor_model_id)
+        self.indoor_model = AutoModelForImageClassification.from_pretrained(self.indoor_model_id)
+        self.indoor_pipe = pipeline(
+            'image-classification',
+            model = self.indoor_model,
+            feature_extractor = self.indoor_feature_extractor,
+        )
+
+        #facial emotion
+        self.facial_emotion_model_id = 'Rajaram1996/FacialEmoRecog'
+        self.facial_emotion_feature_extractor = AutoFeatureExtractor.from_pretrained(self.facial_emotion_model_id)
+        self.facial_emotion_model = AutoModelForImageClassification.from_pretrained(self.facial_emotion_model_id)
+        self.emotion_pipe = pipeline(
+            'image-classification',
+            model = self.facial_emotion_model,
+            feature_extractor = self.facial_emotion_feature_extractor,
+        )
+
+        #caption
+        self.caption_model_id = 'nlpconnect/vit-gpt2-image-captioning'
+        self.caption_feature_extractor = AutoFeatureExtractor.from_pretrained(self.caption_model_id)
+        self.caption_model = AutoModelForVision2Seq.from_pretrained(self.caption_model_id)
+        self.caption_tokenizer = AutoTokenizer.from_pretrained(self.caption_model_id)
+        self.caption_pipe = pipeline(
+            'image-to-text',
+            model = self.caption_model,
+            feature_extractor = self.caption_feature_extractor,
+            tokenizer = self.caption_tokenizer,
+        )
+
+        #places365 - outdoor scene
+        arch = 'resnet50'
+
+        # load the pre-trained weights
+        model_file = 'places365/%s_places365.pth.tar' % arch
+        self.places365_model = models.__dict__[arch](num_classes=365)
+        checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        self.places365_model.load_state_dict(state_dict)
+        self.places365_model.eval()
+
+        self.places365_centre_crop = trn.Compose([
+                trn.Resize((256,256)),
+                trn.CenterCrop(224),
+                trn.ToTensor(),
+                trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+        # load the class label
+        file_name = 'places365/categories_places365.txt'
+        classes = list()
+        with open(file_name) as class_file:
+            for line in class_file:
+                classes.append(line.strip().split(' ')[0][3:])
+        self.places365_classes = tuple(classes)
+
+    def outdoor_pipe(self, image_path):
+        # load the test image
+        img = Image.open(image_path)
+        input_img = V(self.places365_centre_crop(img).unsqueeze(0))
+
+        # forward pass
+        logit = self.places365_model.forward(input_img)
+        h_x = F.softmax(logit, 1).data.squeeze()
+        probs, idx = h_x.sort(0, True)
+
+        # output the prediction
+        results = {'output' : []}
+        for i in range(0, 5):
+            results['output'].append({'score': probs[i].item(), 'label': self.places365_classes[idx[i]]})
+
+        return results
+    
+    def get_results(self, image_path, task_label): 
+        if task_label == 'object detection':
+            return self.object_pipe(image_path)
+        elif task_label == 'indoor scene':
+            return self.indoor_pipe(image_path)
+        elif task_label == 'outdoor scene':
+            return self.outdoor_pipe(image_path)
+        elif task_label == 'facial emotion':
+            return self.emotion_pipe(image_path)
+        elif task_label == 'caption':
+            return self.caption_pipe(image_path)
 
 if __name__=='__main__':
-    image = 'https://farm4.staticflickr.com/3253/2605202065_d1bcaa15b3_z.jpg'
-    model_id = 'nlpconnect/vit-gpt2-image-captioning' 
-    task = 'image-to-text'
+    path_to_image = '../data/MAMI/images/28.jpg'
+    image_models = ImagePipelines()
+    result = image_models.get_results(path_to_image, task_label='caption') 
+    print(result)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-L/14", device=device)
 
-    results = image_pipeline(image, model_id, task)
-    print(results)
+    image = preprocess(Image.open(path_to_image)).unsqueeze(0).to(device)
+    image_types = ["This is a image", "This is a sketch", "This is a cartoon", "This is a painting"] 
+    text = clip.tokenize(image_types).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(text)
+
+        logits_per_image, logits_per_text = model(image, text)
+        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+    
+    print("Label probs:", probs)
--- Stage2/main.py
+++ Stage2/main.py
@@ -3,19 +3,13 @@ sys.path.insert(0, "../")
 import os
 import json
 from Stage1.txt_pipeline import text_pipeline
-from img_pipeline import image_pipeline
+from img_pipeline import ImagePipelines
 import pandas as pd
 
 if __name__=='__main__':
-    image_models = {
-        'object-detection' : ['facebook/detr-resnet-101'],
-        'image-classification' : ['vincentclaes/mit-indoor-scenes', 'Rajaram1996/FacialEmoRecog'],
-        'image-to-text' : ['nlpconnect/vit-gpt2-image-captioning']
-       #'OFA-Sys/ofa-huge' : 'image-to-text',
-    }
     dataset = pd.read_csv('../data/MAMI/trial.csv', delimiter='\t')
-    dataset = dataset[:20]
-
+    dataset = dataset[:3]
+    image_models = ImagePipelines()
     for _, row in dataset.iterrows():
         mami_results = {
             'file' : row[0],
@@ -25,11 +19,10 @@ if __name__=='__main__':
 
         img_path = os.path.join('../data/MAMI/images', row[0])
         
-        for task in image_models:
-            mami_results[task] = []
-            for model in image_models[task]:
-                result = image_pipeline(img_path, model, task)
-                mami_results[task].append({'model_id': model, 'output': result})
+        for task_label in image_models.tasks:
+                print(f'Processing {task_label}')
+                result = image_models.get_results(img_path, task_label)
+                mami_results[task_label] = {'output': result}
 
         with open('mami_results.jsonl', 'a') as f:
             json.dump(mami_results, f)
